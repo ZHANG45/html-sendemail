36644a2b891383255110b96d810863d62ec48371
ZHANG45 <34020288+ZHANG45@users.noreply.github.com>
2018-07-30 12:26:23 +0900

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -12,15 +12,9 @@ $email_address = $_GET['email'];
 $message = $_GET['message'];
 
 // Create the email and send the message
-$to = '451982246@qq.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'receivemail@yourdomain.com'; // Add your email address inbetween the '' replacing receivemail@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact From:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-
-//echo $name, $email_address, $message;
-
-
 
 // This example shows making an SMTP connection with authentication.
 
@@ -30,9 +24,9 @@ use PHPMailer\PHPMailer\Exception;
 //SMTP needs accurate times, and the PHP time zone MUST be set
 //This should be done in your php.ini, but this is how to do it if you don't have access to that
 date_default_timezone_set('Etc/UTC');
-require '/../PHPMailer/PHPMailer/src/Exception.php';
-require '/../PHPMailer/PHPMailer/src/PHPMailer.php';
-require '/../PHPMailer/PHPMailer/src/SMTP.php';
+require 'PHPMailer/PHPMailer/src/Exception.php';
+require 'PHPMailer/PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/PHPMailer/src/SMTP.php';
 //Create a new PHPMailer instance
 $mail = new PHPMailer;
 
@@ -42,30 +36,29 @@ $mail->isSMTP();
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
-$mail->SMTPDebug = 0;
-//Set the hostname of the mail server
-$mail->Host = 'smtp.qq.com';
-//Set the SMTP port number - likely to be 25, 465 or 587
-$mail->Port = 465;
+$mail->SMTPDebug = 2;
+//Set the hostname of the mail server, you need to get it from your mail sever
+$mail->Host = 'xxxxxxxx';   
+//Set the SMTP port number - likely to be 25, 465 or 587, you need to get it from your mail sever
+$mail->Port = 25;
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication
-$mail->Username = '451982246@qq.com';
+$mail->Username = 'sendmail@yourdomain.com'; #your mail address used to send email
 //Password to use for SMTP authentication
-$mail->Password = 'jxvryvvmpvkzcbea'; //
+$mail->Password = '*********'; //you may need the password or AUTHORIZE CODE of your mail account sendmail@yourdomain.com
 //Set who the message is to be sent from
-$mail->setFrom('451982246@qq.com', $name);
+$mail->setFrom('sendmail@yourdomain.com', $name);
 //Set an alternative reply-to address
 $mail->addReplyTo($to, $name);
 //Set who the message is to be sent to
-$mail->addAddress($to, 'ZHANG YING');
+$mail->addAddress($to, 'your name');
 //Set the subject line
 $mail->Subject = $email_subject;
 $mail->Body    = $email_body;
-$mail->SMTPSecure = 'ssl';
+$mail->SMTPSecure = 'ssl'; #if your mail sever use ssl to encrypt the email，like qq
 if($mail->send()){
 	header("Location: thank_you.html");  
-	exit;
 }
 else{
 	echo "Failed, please try agian";
